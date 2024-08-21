cf4193eb157433abbb90d2a702f3adbff6bb717c
sanu-coder <sanya30122000@gmail.com>
2024-08-21 11:40:16 +0530

    commit

--- src/Components/User/ProtectedRoute.jsx
+++ src/Components/User/ProtectedRoute.jsx
@@ -1,6 +1,21 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
+export function getHeaders(){
+    const token = localStorage.getItem('token');
+    const headers = {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json',
+    };
+    return headers;
+}
+
+export function  getFormHeaders(){
+    const token = localStorage.getItem('token');
+    const headers =  { 'Authorization': `Bearer ${token}`, 'Content-Type': 'multipart/form-data'};
+    return headers;
+}
+
 const ProtectedRoute = ({children}) => {
     const navigate = useNavigate();
     useEffect(() => {
